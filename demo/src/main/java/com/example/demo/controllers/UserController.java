651ecd68f0474c726ac2f8d1c22dfeae2c649964
Fargys1879 <63678494+Fargys1879@users.noreply.github.com>
2020-07-03 21:59:59 +0300

    users

---
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import com.example.demo.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+
+        return "userList";
+
+    }
+}
