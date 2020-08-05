028288d5bc0b40978420d7c4441cf4cc449c4e90
Fargys1879 <63678494+Fargys1879@users.noreply.github.com>
2020-08-05 10:13:45 +0300

    users

--- demo/src/main/java/com/example/demo/controllers/UserController.java
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Role;
+import com.example.demo.models.Task;
 import com.example.demo.models.User;
 import com.example.demo.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,49 +9,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserRepository userRepo;
 
-    @GetMapping
+    @GetMapping("/user")
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        Iterable<User> users = userRepo.findAll();
+
+        model.addAttribute("users", users);
         return "userList";
     }
 
-    @GetMapping("{user}")
-    public String userEditForm(@PathVariable User user, Model model) {
-        model.addAttribute("user", user);
+    @GetMapping("/user/{id}/edit")
+    public String userEditForm( @PathVariable(value = "id") Long id,  Model model) {
+        if(!userRepo.existsById(id)) {
+            return "redirect:/user";
+        }
+        Optional<User> user = userRepo.findById(id);
+        ArrayList<User> res = new ArrayList<>();
+        user.ifPresent(res::add);
+        model.addAttribute("user", res);
         model.addAttribute("roles", Role.values());
-
         return "userEdit";
 
     }
 
-    @PostMapping
-    public String userSave(@RequestParam String username,
-                            @RequestParam Map<String, String> form,
-                            @RequestParam("userId") User user) {
-        user.setUsername(username);
-        userRepo.save(user);
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-        user.getRoles().clear();
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-
-        }
+    @PostMapping("/user/{id}/edit")
+    public String userSave(@RequestParam String userName,
+                           @RequestParam String userPassword,
+                           @PathVariable(value = "id") User user,Model model) {
+        user.setUsername(userName);
+        user.setPassword(userPassword);
         userRepo.save(user);
         return "redirect:/user";
     }
